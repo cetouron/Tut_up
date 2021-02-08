3fc7ae0fb6e5abf4e8704cad47ff26ffe464f46a
cetouron <cedric.touron@univ-lyon2.fr>
2021-02-08 17:12:41 +0100

    deploy

---
+++ precache-manifest.dd56349b57faf8fbb1cf4275bde17897.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2dc11ebcffee9ca99d7",
+    "url": "/Tut_up/css/app.3c63f55b.css"
+  },
+  {
+    "revision": "e2dc11ebcffee9ca99d7",
+    "url": "/Tut_up/js/app.fabb51d8.js"
+  },
+  {
+    "revision": "34c6552abee7acad674a",
+    "url": "/Tut_up/css/chunk-vendors.87d6295a.css"
+  },
+  {
+    "revision": "34c6552abee7acad674a",
+    "url": "/Tut_up/js/chunk-vendors.2005599c.js"
+  },
+  {
+    "revision": "bc984efd337df903408b325955566b2e",
+    "url": "/Tut_up/img/down.bc984efd.png"
+  },
+  {
+    "revision": "2f14a30c6d94ddea9c4805f1ec7d0ffe",
+    "url": "/Tut_up/img/reactivite.2f14a30c.png"
+  },
+  {
+    "revision": "1b9404710e0769d81c0d071e7dc9dfe2",
+    "url": "/Tut_up/img/titre.1b940471.jpg"
+  },
+  {
+    "revision": "e6b5de8e170a2e8acaec98979d01feb6",
+    "url": "/Tut_up/img/qqtutup.e6b5de8e.jpg"
+  },
+  {
+    "revision": "2b609093a3c59421e61daae28cbb815d",
+    "url": "/Tut_up/img/transmission.2b609093.png"
+  },
+  {
+    "revision": "8785dbd857ab5ff538e668713f310662",
+    "url": "/Tut_up/img/confiance.8785dbd8.png"
+  },
+  {
+    "revision": "725b33c29df9e5ce5d6324dafac7d763",
+    "url": "/Tut_up/img/chiffresBg.725b33c2.png"
+  },
+  {
+    "revision": "6e85cf6caee12dd0d152df6b7b2604ec",
+    "url": "/Tut_up/img/video.6e85cf6c.jpg"
+  },
+  {
+    "revision": "dd49c6c19b39b0ac23ea78a88c4b918f",
+    "url": "/Tut_up/index.html"
+  },
+  {
+    "revision": "725b33c29df9e5ce5d6324dafac7d763",
+    "url": "/Tut_up/img/chiffresBg.png"
+  },
+  {
+    "revision": "8785dbd857ab5ff538e668713f310662",
+    "url": "/Tut_up/img/confiance.png"
+  },
+  {
+    "revision": "bc984efd337df903408b325955566b2e",
+    "url": "/Tut_up/img/down.png"
+  },
+  {
+    "revision": "e6b5de8e170a2e8acaec98979d01feb6",
+    "url": "/Tut_up/img/qqtutup.jpg"
+  },
+  {
+    "revision": "2f14a30c6d94ddea9c4805f1ec7d0ffe",
+    "url": "/Tut_up/img/reactivite.png"
+  },
+  {
+    "revision": "1b9404710e0769d81c0d071e7dc9dfe2",
+    "url": "/Tut_up/img/titre.jpg"
+  },
+  {
+    "revision": "2b609093a3c59421e61daae28cbb815d",
+    "url": "/Tut_up/img/transmission.png"
+  },
+  {
+    "revision": "6e85cf6caee12dd0d152df6b7b2604ec",
+    "url": "/Tut_up/img/video.jpg"
+  },
+  {
+    "revision": "3fbc86e1ce8b2b46b24c2b1195f2fbb1",
+    "url": "/Tut_up/css/main.min.css"
+  },
+  {
+    "revision": "b4e26d7c081266173ba632ddd386fb51",
+    "url": "/Tut_up/img/products/didier.jpg"
+  },
+  {
+    "revision": "6b79cd34e9ae0a1b615beb0a5e9226b6",
+    "url": "/Tut_up/img/products/gerard.jpg"
+  },
+  {
+    "revision": "998cafef1075ea68d823b350108d7b1b",
+    "url": "/Tut_up/img/products/juliette.jpg"
+  },
+  {
+    "revision": "1b7ecef8151eaa9925d3d5e9ea20b5cf",
+    "url": "/Tut_up/img/products/livre.jpg"
+  },
+  {
+    "revision": "07e9ad73969ba8fc5701057f6d7ae191",
+    "url": "/Tut_up/img/products/logo.png"
+  },
+  {
+    "revision": "15a8cfc4a1e8628f583588a6290d0098",
+    "url": "/Tut_up/img/products/louis.jpg"
+  },
+  {
+    "revision": "3486570f20902d3fac799a59f716e5a7",
+    "url": "/Tut_up/img/products/logomin.png"
+  },
+  {
+    "revision": "15c915d88e5d2c597c2553c9206c4049",
+    "url": "/Tut_up/img/products/murielle.jpg"
+  },
+  {
+    "revision": "5a565281f57b608b33eab0f4d8656729",
+    "url": "/Tut_up/img/products/sophie.jpeg"
+  },
+  {
+    "revision": "851ad3434deccb220bfeaa5181fea69d",
+    "url": "/Tut_up/img/valeur1.png"
+  },
+  {
+    "revision": "23e2d4cb719a3b670b15b8d6ea164d8b",
+    "url": "/Tut_up/img/products/thibault.jpg"
+  },
+  {
+    "revision": "ba16a66f33e32dd5b64f2d8d909ff5e1",
+    "url": "/Tut_up/img/valeur2.png"
+  },
+  {
+    "revision": "463424daaace7d3c39dd462c5686bbfb",
+    "url": "/Tut_up/img/valeur3.png"
+  },
+  {
+    "revision": "5dac7db97a13bb3b56591b7e02adf43a",
+    "url": "/Tut_up/img/products/sebastien.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Tut_up/robots.txt"
+  },
+  {
+    "revision": "9d081dc651be3b6fa40a7515832aa30f",
+    "url": "/Tut_up/scss/main.scss"
+  },
+  {
+    "revision": "5b034d3ebafed8da2993e95ba71c9034",
+    "url": "/Tut_up/img/products/product-2.jpg"
+  },
+  {
+    "revision": "0df280d6f1d2bff4f9cd5243625aab6b",
+    "url": "/Tut_up/img/products/product-5.jpg"
+  },
+  {
+    "revision": "21cbc7848fb1fc18d05086d77fd170f1",
+    "url": "/Tut_up/img/products/product-3.jpg"
+  },
+  {
+    "revision": "127ec8b7c68d846b499d360008238d4a",
+    "url": "/Tut_up/img/products/product-1.jpg"
+  },
+  {
+    "revision": "ba25e4a9cb376e1902acbdcff0396101",
+    "url": "/Tut_up/img/products/product-4.jpg"
+  },
+  {
+    "revision": "e317c339bb46c243d3e34ec8481e30b9",
+    "url": "/Tut_up/img/products/product-6.jpg"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Tut_up/precache-manifest.dd56349b57faf8fbb1cf4275bde17897.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "awesome-store"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
