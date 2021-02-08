bc6ea26d3ace71078719569a76f015469660b43c
cetouron <cedric.touron@univ-lyon2.fr>
2021-02-08 17:11:30 +0100

    phrase

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,4 @@
-module.exports = {
-  "transpileDependencies": [
-      "vuetify"
-  ]
-}
+
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/Tut_up/" : "/",
   transpileDependencies: ["vuetify"],
